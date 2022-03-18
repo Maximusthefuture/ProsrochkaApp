a3b1114142378b675e0016acd74382ec8dc38532
MaximusTheFuture <naturemade@yandex.ru>
2022-03-18 12:25:18 +0300

    created coredatastack, created product entity

---
+++ ProsrochkaApp/CoreData/Product+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Product+CoreDataClass.swift
+//  ProsrochkaApp
+//
+//  Created by Maximus on 18.03.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Product)
+public class Product: NSManagedObject {
+
+}
---
+++ ProsrochkaApp/CoreData/Product+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Product+CoreDataProperties.swift
+//  ProsrochkaApp
+//
+//  Created by Maximus on 18.03.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Product {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Product> {
+        return NSFetchRequest<Product>(entityName: "Product")
+    }
+
+    @NSManaged public var createdDate: Date?
+    @NSManaged public var expiredDate: Date?
+    @NSManaged public var name: String?
+    @NSManaged public var productDescription: String?
+    @NSManaged public var quantity: Int16
+    @NSManaged public var tags: [String]?
+    @NSManaged public var image: Data?
+
+}
